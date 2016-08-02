1ff49e3ddc7921abfc9c659779ecc20453cf7b22
johanbissemattsson <johan@johanbissemattsson.se>
2016-08-02 16:44:20 +0200

    Add smoothscroll in articlejs

--- app/containers/Article.js
+++ app/containers/Article.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from '../stylesheets/containers/article.css';
 import { StickyContainer, Sticky } from 'react-sticky';
+import smoothScroll from 'smoothscroll';
 
 export default class Article extends React.Component {
   render() {
