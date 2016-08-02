5b0f16fb1a7c8af0380e259cb23ba6c69df396d9
johanbissemattsson <johan@johanbissemattsson.se>
2016-08-02 16:42:43 +0200

    Add React-sticky

--- app/containers/Article.js
+++ app/containers/Article.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from '../stylesheets/containers/article.css';
+import { StickyContainer, Sticky } from 'react-sticky';
 
 export default class Article extends React.Component {
   render() {
