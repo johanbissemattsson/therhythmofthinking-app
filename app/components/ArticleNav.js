db7f07b217c4d036053d0ff0a23da3e955739200
johanbissemattsson <johan@johanbissemattsson.se>
2016-08-02 16:47:25 +0200

    Add article nav

---
+++ app/components/ArticleNav.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router';
+import styles from '../stylesheets/components/articleNav.css';
+
+export default class ArticleNav extends React.Component {
+  render() {
+    return (
+      <p>ArticleNav</p>
+    )
+  }
+}
--- app/containers/Article.js
+++ app/containers/Article.js
@@ -2,11 +2,12 @@ import React from 'react';
 import styles from '../stylesheets/containers/article.css';
 import { StickyContainer, Sticky } from 'react-sticky';
 import smoothScroll from 'smoothscroll';
+import ArticleNav from '../components/ArticleNav';
 
 export default class Article extends React.Component {
   render() {
     return (
-      <p>Article</p>
+      <ArticleNav />
     );
   }
 }
